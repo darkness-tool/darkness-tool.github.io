8b7a5bd4af9eedf00135db7a6e341ff0df44b21b
darkness-tool <47018909+darkness-tool@users.noreply.github.com>
2024-10-05 21:01:27 +0800

    update banner

--- load_banner.js
+++
@@ -1,5 +0,0 @@
-
-$(document).ready(function() {
-	$('.banner').append("<div id=\"banner-index\"></div>");
-	$('#banner-index').load("banner.html");
-});
--- load_index.js
+++ load_index.js
@@ -2,4 +2,6 @@
 $(document).ready(function() {
 	$('.container').append("<div id=\"index\"></div>");
 	$('#index').load("indexContent.html");
+	$('.container').append("<div id=\"banner-index\"></div>");
+	$('#banner-index').load("banner.html");
 });
